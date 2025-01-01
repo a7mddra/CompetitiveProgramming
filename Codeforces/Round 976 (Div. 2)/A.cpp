da04cabebbc5691d852b7fdb8d5c3226cc9d734f
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-01 02:10:35 +0200

    Create A.cpp

---
+++ Codeforces/Round 976 (Div. 2)/A.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+int main()
+{
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int n, k, op = 0;
+        cin >> n >> k;
+        if (k==1) {
+            cout << n << endl;
+        }
+        else { while (n > 0) {
+                op += n % k;
+                n /= k;
+            
+        }
+        cout << op << endl;
+            
+        }
+    }
+    return 0;
+}
