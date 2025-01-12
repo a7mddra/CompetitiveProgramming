5cd20cbf1f28d2f0446d756e3c63e315571815da
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-12 06:26:50 +0200

    Update B.cpp

--- Codeforces/Round 776 (Div. 3)/B.cpp
+++ Codeforces/Round 776 (Div. 3)/B.cpp
@@ -1,1 +1,51 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/1650/B
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int l, r, a, max_mod, max_div, idx;
+    cin >> l >> r >> a;
+
+    int st = l % a;
+    int cnt = (r - l + 1) / a;
+    int rem = (r - l + 1) % a;  
+
+    if (rem > 0) {
+        int end = (st + rem - 1) % a;
+        if (st <= end) {
+            if (a - 1 >= st && a - 1 <= end) {
+                cnt++;
+            }
+        } else {
+            if (a - 1 >= st || a - 1 <= end) {
+                cnt++;
+            }
+        }
+    }
+
+    if (cnt == 0) {
+        max_mod = l%a + r-l;
+        idx = r-l+1;
+    }
+    
+    if (cnt > 0) {
+        max_mod = a-1;
+        idx = max_mod - l%a + (cnt-1) * a;
+    }
+    
+    max_div = (l+idx) / a;
+    
+    cout << max_div + max_mod <<endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    } return 0;
+}
