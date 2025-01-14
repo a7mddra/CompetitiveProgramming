791de337ddbbaf18ec4b2d1672b7b8d2f4aac8c8
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-14 07:04:36 +0200

    Update A.cpp

--- Codeforces/Round 135 (Div. 2)/A.cpp
+++ Codeforces/Round 135 (Div. 2)/A.cpp
@@ -1,1 +1,29 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/1728/A
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+	int n, e, idx, max=0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+	    cin >> e;
+	    if (e>max) {
+	        max=e;
+	        idx=i+1;
+	    }
+	}
+	cout << idx << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
