825266470bc6d298c5600538426f20493d9fbcff
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-20 18:32:46 +0200

    Update A.cpp

--- Codeforces/Round 996 (Div. 2)/A.cpp
+++ Codeforces/Round 996 (Div. 2)/A.cpp
@@ -1,1 +1,19 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2055/A
+*************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve() 
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << (abs(a-b)&1?"nO":"yEs") << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
