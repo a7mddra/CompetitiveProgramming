0f5cbede5df23cc0d328d952be209da79bed5d5d
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-21 03:47:49 +0200

    Update C.cpp

--- Codeforces/Round 997 (Div. 2)/C.cpp
+++ Codeforces/Round 997 (Div. 2)/C.cpp
@@ -1,1 +1,29 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2056/C
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+	int n;
+	cin >> n;
+    string str = "1";
+
+    for (int i = 1; i < n - 1; ++i) {
+        str += " " + to_string(i);
+    }
+
+    str += " 1";
+    cout << str << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
