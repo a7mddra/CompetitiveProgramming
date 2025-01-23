7e352b447cea94e42aa5f0af26dc44110a5e7a6b
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-23 23:36:44 +0200

    Update A.cpp

--- Codeforces/Round 849 (Div. 4)/A.cpp
+++ Codeforces/Round 849 (Div. 4)/A.cpp
@@ -1,1 +1,29 @@
+/**********************************************
+ https://codeforces.com/contest/1791/problem/A
+***********************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+	char c;
+	cin >> c;
+	string cf = "cdefors";
+	for(char x:cf){
+	    if(c==x){
+	        cout<<"yEs"<<endl;
+	        return;
+	    }
+	}cout<<"nO"<<endl;
+	
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
