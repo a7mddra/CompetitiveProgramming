cb85b1560087dd12950180403b9b8ff97190eb5e
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-24 14:26:14 +0200

    Create sample.cpp

---
+++ Codeforces/Draft/sample.cpp
@@ -0,0 +1,20 @@
+/*************************************************
+ https://
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve()
+{
+    int n,
+    
+    cout <<  << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+	int t;  cin >> t;
+	while(t--) solve(); return 0;
+}
