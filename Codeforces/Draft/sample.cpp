afb98492b3439d3c15a3139b18309d15c18456e0
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-26 15:55:21 +0200

    Update sample.cpp

--- Codeforces/Draft/sample.cpp
+++ Codeforces/Draft/sample.cpp
@@ -1,5 +1,5 @@
 /*************************************************
- https://
+ 
 **************************************************/
 #include "bits/stdc++.h"
 using namespace std;
@@ -7,14 +7,14 @@ using namespace std;
 void solve()
 {
     int n,
-    
-    cout <<  << endl;
+    cin >> n;
+    cout <<  << '\n';
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);    
-	int t;  cin >> t;
-	while(t--) solve(); return 0;
+    int t;  cin >> t;
+    while(t--) solve(); return 0;
 }
