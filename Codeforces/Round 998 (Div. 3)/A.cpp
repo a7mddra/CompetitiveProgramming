be64ad2b57fb7c35a868b589430cdec1d266ff80
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-26 15:54:11 +0200

    Update A.cpp

--- Codeforces/Round 998 (Div. 3)/A.cpp
+++ Codeforces/Round 998 (Div. 3)/A.cpp
@@ -1,1 +1,24 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2060/A
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int a, b, d, e;
+    cin >> a >> b >> d >> e;
+    set<int> s;
+    s.insert(a+b);
+    s.insert(d-b);
+    s.insert(e-d);
+    cout << 4 - s.size() << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+	int t;  cin >> t;
+	while(t--) solve(); return 0;
+}
