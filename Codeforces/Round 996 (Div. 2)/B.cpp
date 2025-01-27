436f51cc8a21d82804588cb8771183b4beacd80f
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-28 00:48:26 +0200

    Update B.cpp

--- Codeforces/Round 996 (Div. 2)/B.cpp
+++ Codeforces/Round 996 (Div. 2)/B.cpp
@@ -1,1 +1,29 @@
+/**********************************************
+ https://codeforces.com/contest/2055/problem/B
+***********************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int& e:a) cin >> e;
+    for (int i=0; i<n; ++i) {
+        cin >> b[i];
+        a[i] -= b[i];
+    }
+    sort(a.begin(), a.end());
+    
+    cout << (a[0]+a[1]>=0?"yEs":"nO") << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
