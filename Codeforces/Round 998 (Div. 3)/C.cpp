6a71746f854a3674df5d9dbadabec6a3256c5e81
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-27 11:50:31 +0200

    Update C.cpp

--- Codeforces/Round 998 (Div. 3)/C.cpp
+++ Codeforces/Round 998 (Div. 3)/C.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> b(n);
+    for (int& e : b) cin >> e;
+    sort(b.begin(), b.end());
+    int l = 0, r = n - 1;
+    int ans = 0;
+    while (l < r) {
+        int sum = b[l] + b[r];
+        if (sum == k) {
+            ans++;
+            l++;
+            r--;
+        } else if (sum < k) l++;
+        else r--;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
