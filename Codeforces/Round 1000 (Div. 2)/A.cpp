eceee4ddc173c196e5a4d812cc47c613ea7e5eee
Ahmed Ramadan <a7mddra@gmail.com>
2025-01-30 03:05:01 +0200

    Update A.cpp

--- Codeforces/Round 1000 (Div. 2)/A.cpp
+++ Codeforces/Round 1000 (Div. 2)/A.cpp
@@ -1,1 +1,20 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2063/A
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int l, r;
+    cin >> l >> r;
+    cout << ((l==r&&l==1)?1:r-l) << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
