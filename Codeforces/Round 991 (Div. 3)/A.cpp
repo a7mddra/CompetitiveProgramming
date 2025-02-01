1c210702f3f00f7820744fd7d225b2ebabfd2f2e
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-01 12:03:15 +0200

    Update A.cpp

--- Codeforces/Round 991 (Div. 3)/A.cpp
+++ Codeforces/Round 991 (Div. 3)/A.cpp
@@ -1,1 +1,31 @@
+/*************************************************
+ https://codeforces.com/proslemset/proslem/2050/A
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int n, m, x=0;
+    cin >> n >> m;
+    string w;
+    vector<int> s(n);
+    for (int& e : s) {
+        cin >> w;
+        e = w.size();
+    }
+    for (int i=0; i<n; ++i) {
+        m -= s[i];
+        if (m<0) break;
+        x++;
+    }
+    cout << x << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
