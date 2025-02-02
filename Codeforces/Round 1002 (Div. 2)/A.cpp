bb4345e6dd4e055bdebad8e948ccea3b59b627ac
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-02 18:43:01 +0200

    Update A.cpp

--- Codeforces/Round 1002 (Div. 2)/A.cpp
+++ Codeforces/Round 1002 (Div. 2)/A.cpp
@@ -1,1 +1,30 @@
+/**********************************************
+ https://codeforces.com/contest/2059/problem/A
+***********************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int n, e;
+    cin >> n;
+    set<int> s1;
+    set<int> s2;
+    for (int i=0; i<n; ++i) {
+        cin >> e;
+        s1.insert(e);
+    }
+    for (int i=0; i<n; ++i) {
+        cin >> e;
+        s2.insert(e);
+    }    
+    cout << ((s1.size()+s2.size())>3?"yEs":"nO") << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
