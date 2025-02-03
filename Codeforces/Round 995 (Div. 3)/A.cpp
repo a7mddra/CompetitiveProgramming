c858778096bd0459ae3fea4d8dd03b9cc8dbf2ef
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-03 11:32:09 +0200

    Update A.cpp

--- Codeforces/Round 995 (Div. 3)/A.cpp
+++ Codeforces/Round 995 (Div. 3)/A.cpp
@@ -1,1 +1,27 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2051/A
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int n, cnt=0;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int& e : a) cin >> e;
+    for (int& e : b) cin >> e;
+    for (int i=0;i <n-1; ++i) {
+        if (a[i]>b[i+1]) cnt += a[i]-b[i+1];
+    }
+    cout << cnt + a[n-1] << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
