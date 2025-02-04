dcf6891934609b2df3dc554af6ca8c9ee296eef1
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-04 16:16:52 +0200

    Update B.cpp

--- Codeforces/Round 993 (Div. 4)/B.cpp
+++ Codeforces/Round 993 (Div. 4)/B.cpp
@@ -1,1 +1,25 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2044/B
+**************************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    string in, out;
+    cin >> in;
+    for (int i=in.size()-1; i>-1; --i) {
+        if (in[i]=='q') in[i] = 'p';
+        else if (in[i]=='p') in[i] = 'q';
+        out+=in[i];
+    }
+    cout << out << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
