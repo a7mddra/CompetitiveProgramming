6b618711252c22c84cb4b7a21533f35bd2e0fea3
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-05 14:18:17 +0200

    Update A.cpp

--- Codeforces/Round 993 (Div. 4)/A.cpp
+++ Codeforces/Round 993 (Div. 4)/A.cpp
@@ -1,1 +1,20 @@
+/**********************************************
+ https://codeforces.com/contest/2044/problem/A
+***********************************************/
+#include "bits/stdc++.h"
+using namespace std;
 
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n-1 << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int t;  cin >> t;
+    while (t--) solve(); return 0;
+}
