3398bae9b7deb436bead07325300d710c94d4082
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-07 02:05:50 +0200

    Update A.cpp

--- Codeforces/Round 1001 (Div. 1, 2)/A.cpp
+++ Codeforces/Round 1001 (Div. 1, 2)/A.cpp
@@ -1,1 +1,24 @@
+/*************************************************
+ https://codeforces.com/problemset/problem/2062/A
+**************************************************/
+#include "bits/stdc++.h"
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    ll cnt=0LL;
+    string s;
+    cin >> s;
+    for (char c:s) cnt+= c-'0';
+    cout << cnt << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    ll t;  cin >> t;
+    while (t--) solve(); return 0;
+}
 
