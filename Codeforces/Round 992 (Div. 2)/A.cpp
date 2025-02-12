4d4e1ab69b62bab774600c216cf6291bfa7c1263
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-13 01:07:12 +0200

    Update A.cpp

--- Codeforces/Round 992 (Div. 2)/A.cpp
+++ Codeforces/Round 992 (Div. 2)/A.cpp
@@ -1,1 +1,37 @@
+/**********************************************
+ https://codeforces.com/contest/2040/problem/A
+***********************************************/
+#include "bits/stdc++.h"
+#define ll long long
+using namespace std;
 
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll& e : a) cin >> e;
+    for (ll i=0; i<n; ++i) {
+        bool f=true;
+        for (ll j=0; j<n; ++j) {
+            if (i==j) continue;
+            if (abs(a[i]-a[j])%k==0){
+                f=false;
+                break;
+            } 
+        }
+        if (f) {
+            cout << "yEs\n" << i+1 << '\n';
+            return;
+        }
+    }
+    cout << "nO" << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    ll t;  cin >> t;
+    while (t--) solve(); return 0;
+}
