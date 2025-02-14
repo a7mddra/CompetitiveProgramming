81b59c6541aad53d961a1b0b439d13d2a9e055bc
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-14 02:11:52 +0200

    Update A.cpp

--- Codeforces/Round 173 (Div. 2)/A.cpp
+++ Codeforces/Round 173 (Div. 2)/A.cpp
@@ -1,1 +1,25 @@
+/**********************************************
+ https://codeforces.com/contest/2043/problem/A
+***********************************************/
+#include "bits/stdc++.h"
+#define ll long long
+using namespace std;
 
+void solve()
+{
+    ll n, x=1;
+    cin >> n;
+    while (n>3) {
+        n/=4;
+        x*=2;
+    }
+    cout << x << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    ll t;  cin >> t;
+    while (t--) solve(); return 0;
+}
