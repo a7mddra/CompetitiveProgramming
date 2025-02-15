b0e51f29c42181eb6527e613e243fbcfd275f5b5
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-15 20:29:34 +0200

    Update C.cpp

--- Codeforces/Round 988 (Div. 3)/C.cpp
+++ Codeforces/Round 988 (Div. 3)/C.cpp
@@ -1,1 +1,27 @@
+/**********************************************
+ https://codeforces.com/contest/2037/problem/C
+***********************************************/
+#include "bits/stdc++.h"
+#define ll long long
+using namespace std;
+
+void solve(){
+  ll n;
+  cin >> n;
+  if (n <= 4) cout << -1 << '\n';
+  else {
+    cout << "1 3 ";
+    for (ll i = 7; i <= n; i += 2) cout << i << " ";
+    cout << "5 4 2 ";
+    for (ll i = 6; i <= n; i += 2) cout << i << " ";
+  }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    ll t;  cin >> t;
+    while (t--) solve(); return 0;
+}
 
