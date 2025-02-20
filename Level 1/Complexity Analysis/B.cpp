3823cdbac0ac664150663a70e124b941c99197fd
Ahmed Ramadan <a7mddra@gmail.com>
2025-02-21 00:58:47 +0200

    Create B.cpp

---
+++ Level 1/Complexity Analysis/B.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    string s, t;
+    cin>>s;
+    ll n, cnt0=0, cnt1=0, cnt2=0, MIN, MAX;
+    cin>>n;
+    for (char c:s) 
+        c=='?'?++cnt0:c=='*'?++cnt1:++cnt2;
+    MIN=cnt2-cnt1-cnt0;
+    MAX=cnt1>0?1e9:cnt2;
+    if (n<MIN||n>MAX) {
+        cout << "Impossible";
+        return;
+    }
+    for (ll i=0; i<s.size()&&t.size()<n; ++i) {
+        if(s[i]=='?'||s[i]=='*') continue;
+        else {
+            if (i+1<s.size()&&s[i+1]=='*') {
+                if (n>cnt2) {
+                    ll j=i;
+                    while (j<n) {
+                        t+=s[i];
+                        ++j;
+                    }
+                    cout <<t;
+                    return;
+                } else if (n<cnt2) --cnt2;
+                else t+=s[i];
+            } else if (i+1<s.size()&&s[i+1]=='?') {
+                if (n<cnt2) --cnt2;
+                else t+=s[i];
+            } else t+=s[i];
+        }
+    }
+    cout << t;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    ll t=1;//  cin >> t;
+    while (t--) solve(); return 0;
+}
