3ac6ab073991ed5eef120614210487350f7c6194
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-01 21:18:28 +0200

    Create power.cpp

---
+++ Codeforces/Draft/power.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+#define ll long long
+const ll MOD = 1e9+7;
+using namespace std;
+
+
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp /= 2;
+    }
+    return res;
+}
