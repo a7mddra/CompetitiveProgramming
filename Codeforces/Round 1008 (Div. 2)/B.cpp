56ceacc8605e45067a1a4a0485a5c5715686c875
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-11 14:23:37 +0200

    Update B.cpp

--- Codeforces/Round 1008 (Div. 2)/B.cpp
+++ Codeforces/Round 1008 (Div. 2)/B.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    for (int test = 0; test < t; ++test) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n + 1);
+        a[n] = n - 1;
+        a[n - 1] = n;
+        int target = (k % 2 == 1) ? n : n - 1;
+        for (int i = 1; i <= n - 2; ++i) {
+            a[i] = target;
+        }
+        for (int i = 1; i <= n; ++i) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
