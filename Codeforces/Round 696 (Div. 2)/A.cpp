2fc50ba193a39110a352a267f6c177960e3deacf
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-17 23:47:49 +0200

    Update A.cpp

--- Codeforces/Round 696 (Div. 2)/A.cpp
+++ Codeforces/Round 696 (Div. 2)/A.cpp
@@ -1,1 +1,49 @@
+/*
+ *  Author: a7mddra
+ */
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+string el = "\n";
 
+void ip(bool INTERACTIVE) {
+#if LOCAL
+    if (!INTERACTIVE)
+    freopen("input.txt", "r", stdin);
+#endif 
+}
+
+void solve()
+{
+    ll n, d=-1;
+    string s;
+    cin >> n >> s;
+    for (char c:s) {
+        if (c=='1') {
+            if (d!=2) {
+                cout << 1;
+                d=2;
+            } else {
+                cout << 0;
+                d=1;
+            }
+        } else {
+            if (d!=1) {
+                cout << 1;
+                d=1;
+            } else {
+                cout << 0;
+                d=0;
+            }
+        }
+    } cout << el;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); ip(0);
+    ll T = 1; cin >> T;
+    while (T--) solve();
+    return 0;
+}
