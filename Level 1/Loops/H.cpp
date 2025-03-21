716e293c55428db10b21f8b300883dc911c06dc6
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 13:10:40 +0200

    Create H.cpp

---
+++ Level 1/Loops/H.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int rows = 5;
+    const int cols = 5;
+
+    int values[rows][cols];
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> values[i][j];
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if(values[i][j] == 1){
+                cout<<(abs(i-2)+abs(j-2))<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
