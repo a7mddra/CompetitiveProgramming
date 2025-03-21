8d5ff4a1f74a2472cd95a20a27246ab32b6db571
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 12:06:32 +0200

    Create B.cpp

---
+++ Level 1/Basics/B.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    long long l;
+    cin >> l;
+    char c;
+    cin >> c;
+    float f;
+    cin >> f;
+    double d;
+    cin >> d;
+    cout << i << '\n' << l << '\n' << c << '\n' << f << '\n' << d;
+    return 0;
+}
