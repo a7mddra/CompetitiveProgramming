058577bc255a3b21833e36a771596add23babc02
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 13:01:26 +0200

    Create D.cpp

---
+++ Level 1/Conditions/D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+    if(2 < A && A%2 == 0){
+        cout<<"YES"<<endl;
+        
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
