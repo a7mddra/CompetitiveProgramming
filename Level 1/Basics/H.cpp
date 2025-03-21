af33056c0cb8cc739b466f198dc98ba6d1352508
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 12:47:20 +0200

    Create H.cpp

---
+++ Level 1/Basics/H.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, m, z;
+    cin >> n >> m >> z;
+    long long d1 = n - m + z;
+    
+    cout << d1;
+
+
+    return 0;
+}
