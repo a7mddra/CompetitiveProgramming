bc7e3944c64537a15361e6a72dd43e3353ab883e
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 12:52:23 +0200

    Create J.cpp

---
+++ Level 1/Basics/J.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, z;
+    cin >> n >> m >> z;
+    int d1 = 7 - n;
+    int d2 = 7 - m;
+    int d3 = 7 - z;
+    
+    cout << d1+d2+d3;
+
+
+    return 0;
+}
