c57672cd2c5528a924fbc0d4c5f836657afddb98
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 13:52:55 +0200

    Create F.cpp

---
+++ Level 1/Bulit-In Functions/F.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0 ; i < n ; i++) {
+        cin >> x;
+        arr.push_back(x);
+    }
+    sort(arr.begin(), arr.end());
+    for (int i = 0 ; i < n ; i++) {
+        cout << arr[i] << " ";
+    }    
+    return 0;
+}
