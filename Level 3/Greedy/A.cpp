37ba0bd13ee5e2d9d081ff33581fb8a5ea5689fe
Ahmed Ramadan <a7mddra@gmail.com>
2025-03-21 11:08:40 +0200

    Create A.cpp

---
+++ Level 3/Greedy/A.cpp
@@ -0,0 +1,36 @@
+/*
+ *  Author: a7mddra
+ */
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+string el = "\n";
+
+void ip(bool INTERACTIVE) {
+#if LOCAL
+    if (!INTERACTIVE)
+    freopen("input.txt", "r", stdin);
+#endif 
+}
+
+void solve()
+{
+    ll n, m, e, ans=0;
+    cin >> n >> m;
+    vector<ll> a(n);
+    for (ll i=0; i<n; ++i) {
+    cin >> e;
+    if (e<0) a[i]=e; }
+    sort(a.begin(), a.end());
+    for (ll i=0; i<m; ++i) ans+=abs(a[i]);
+    cout << ans << el;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); ip(0);
+    ll T = 1;// cin >> T;
+    while (T--) solve();
+    return 0;
+}
