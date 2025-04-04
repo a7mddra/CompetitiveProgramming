4dd6f92577db8f529a82479f2e5ec59149f6fcf3
Ahmed Ramadan <a7mddra@gmail.com>
2025-04-04 14:01:36 +0200

    Create A.cpp

---
+++ Codeforces/Round 177 (Div. 2)/A.cpp
@@ -0,0 +1,32 @@
+/*
+ *  Author: a7mddra
+ */
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+using ld = long double;
+const ll INF = 1e18;
+string el = "\n";
+
+void ip(bool INTERACTIVE) {
+#if LOCAL
+    if (!INTERACTIVE)
+    freopen("input.txt", "r", stdin);
+#endif 
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << 2*n << el;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); ip(0);
+    ll T = 1; cin >> T;
+    while (T--) solve();
+    return 0;
+}
