1a0ed067cc8105daa32a4becd54937d30dfece07
Ahmed Ramadan <a7mddra@gmail.com>
2025-07-29 15:08:02 +0300

    Update main.cpp

--- Setup/main.cpp
+++ Setup/main.cpp
@@ -4,7 +4,11 @@
 #include "bits/stdc++.h"
 using namespace std;
 using ll = long long;
+using ld = long double;
 string el = "\n";
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+// const ll MOD = 998244353;
 
 void ip(bool INTERACTIVE) {
 #if LOCAL
@@ -13,16 +17,17 @@ void ip(bool INTERACTIVE) {
 #endif 
 }
 
-void solve()
+void solve(ll tc)
 {
 
 }
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0); ip(0);
-    ll T = 1;// cin >> T;
-    while (T--) solve();
-    return 0;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    ll T=1, i=0; cin.tie(0); ip(0);
+    // cin >> T;
+    #define fast i<T      
+    // #define fast cin.peek() != EOF
+    while (fast) {solve(i+1);i++;} return 0;
 }
